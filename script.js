3a3459ccc09c4074bfa6d25feaa312c5e52ef671
jgordner97 <57602353+jgordner97@users.noreply.github.com>
2020-10-21 09:41:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -31,3 +31,12 @@ function printimage(){
 }
 
 crosswordPrint.addEventListener("click", printimage);
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
