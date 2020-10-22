c4a273753a597c3b2bdbcc31ec7d5ba9b1b89a1c
jgordner97 <57602353+jgordner97@users.noreply.github.com>
2020-10-22 16:38:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -32,17 +32,7 @@ tableBtns.forEach(function (btn){
     })
 })
 
-const crosswordPrint = document.querySelector(".crossword__print")
 
-function printimage(){
-    var URL = "https://crosswordlabs.com/embed/superfund-halloween-tour-2";
-
-    var W = window.open(URL);
-
-    W.window.print();
-}
-
-crosswordPrint.addEventListener("click", printimage);
 
 window.onscroll = function() {myFunction()};
 
